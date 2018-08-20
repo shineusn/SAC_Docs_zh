9e609bc35f64a50f3ece9005a40acb72c8b36c9e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-08-20 14:44:32 +0000

    Deploy seisman/SAC_Docs_zh to github.com/seisman/SAC_Docs_zh.git:gh-pages

---
+++ _static/documentation_options.js
@@ -0,0 +1,9 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: '4.0-dev',
+    LANGUAGE: 'zh_CN',
+    COLLAPSE_INDEX: false,
+    FILE_SUFFIX: '',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt'
+};
